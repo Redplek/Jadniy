34e797e3a450d65fddb3bfd2fa7cd6cc38197049
Redplek <Andreydeyneko1994@gmail.com>
2018-04-06 17:59:46 +0300

    рабочая версия

---
+++ Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.Резка = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Остаток = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(202, 202);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(37, 25);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(262, 150);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(37, 205);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Резка,
+            this.Остаток});
+            this.dataGridView2.Location = new System.Drawing.Point(37, 276);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(457, 150);
+            this.dataGridView2.TabIndex = 5;
+            // 
+            // Резка
+            // 
+            this.Резка.HeaderText = "";
+            this.Резка.Name = "Резка";
+            // 
+            // Остаток
+            // 
+            this.Остаток.HeaderText = "";
+            this.Остаток.Name = "Остаток";
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column3});
+            this.dataGridView3.Location = new System.Drawing.Point(523, 25);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(240, 401);
+            this.dataGridView3.TabIndex = 7;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "";
+            this.Column3.Name = "Column3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(797, 497);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Резка;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Остаток;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+             
+            InitializeComponent();
+
+            dataGridView1.RowCount = 3;
+            dataGridView1.ColumnCount = 2;
+
+            dataGridView1.Rows[0].Cells[0].Value = "1065";
+            dataGridView1.Rows[0].Cells[1].Value = "60";
+            dataGridView1.Rows[1].Cells[0].Value = "1080";
+            dataGridView1.Rows[1].Cells[1].Value = "20";
+            dataGridView1.Rows[2].Cells[0].Value = "420";
+            dataGridView1.Rows[2].Cells[1].Value = "40";
+
+            textBox1.Text = "2150";           
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView2.Rows.Clear();
+            int k = 0;
+            int lin = dataGridView1.RowCount;
+            List<int> matrix = new List<int>();
+            for (int j = 0; j < lin; j++)
+            {                
+                    matrix.Add(Convert.ToInt32(dataGridView1.Rows[j].Cells[0].Value));   // получаем массив с длинами
+            }
+
+            List<int> matrix2 = new List<int>();
+            for (int j = 0; j < lin; j++)
+            {
+                matrix2.Add(Convert.ToInt32(dataGridView1.Rows[j].Cells[1].Value));   // получаем массив количеств
+            }
+            /*сортировка пузырьком*/
+            List<int> BubbleSort(List<int> A)
+            {
+                for (int i = 0; i < A.Count; i++)
+                {
+                    for (int j = 0; j < A.Count - i - 1; j++)
+                    {
+                        if (A[j] < A[j + 1])
+                        {
+                            int temp = A[j];
+                            A[j] = A[j + 1];
+                            A[j + 1] = temp;
+                        }
+                    }
+                }
+                return A;
+            }
+            /*сортировка пузырьком конец*/
+
+            List<int> aArr = new List<int>();
+            List<bool> bArr = new List<bool>();
+            for (int i = 0; i < lin; i++)
+            {
+                if(matrix[i] > 0)
+                {
+                    for(int j = 0; j < matrix2[i]; j++)
+                    {                       
+                        aArr.Add(matrix[i]);
+                        bArr.Add(false);
+                        k = k + 1;
+                    }
+                }
+            }
+            aArr = BubbleSort(aArr);
+            bool bFlag = false;
+            int l = 0;
+            int s = 0;            
+            int s1 = Convert.ToInt32(textBox1.Text);
+            List<int> results = new List<int>();
+            List<int> ostatok = new List<int>();
+            List<int> kold = new List<int>();
+            List<string> GLres = new List<string>();
+            List<string> GLost = new List<string>();
+            List<int>[] mas = new List<int>[k];
+            //Debugger.Break();
+            for (int p = 0; p < k; p++)
+            {
+                bFlag = false;
+                l = 0;
+                s = 0;
+                results.Clear();
+                ostatok.Clear();
+                for(int y = 0; y < bArr.Count; y++)
+                {
+                    bArr[y] = false;
+                }
+                if (p > 0)
+                { 
+                    aArr = newaArr(aArr);                    
+                }
+                do
+                {
+                    //Debugger.Break();
+                    ostatok.Add(s);                    
+                    s = s1;
+                    bFlag = false;
+                    l = l + 1;
+                    results.Add(l);
+                    for (int i = 0; i < k; i++)
+                    {
+                        if (s >= aArr[i] && bArr[i] == false)
+                        {
+                            s = s - aArr[i];
+                            results.Add(aArr[i]);
+                            bArr[i] = true;
+                            bFlag = true;
+                        }
+                    }
+
+                } while (bFlag && s < s1);
+                // kold.Add(l);
+                // Debugger.Break();               
+
+
+                //label2.Text = Convert.ToString(l - 1);
+
+                /*обработка и вывод результатов*/
+                int q = 1;
+                results.RemoveAt(results.Count - 1);                
+                dataGridView2.RowCount = 1000;
+                dataGridView2.ColumnCount = 500;
+                int sum = 0;
+                foreach (int el in ostatok)
+                {
+                    sum = sum + el;
+                }               
+                dataGridView2.Rows[0].Cells[p].Value = 1;
+                foreach (int el in results)
+                {
+                    dataGridView2.Rows[q].Cells[p].Value = el;                    
+                    q = q + 1;
+                }
+                results.Add(l-1);/*количество деталей*/
+                results.Add(sum);/*сумма остатков*/
+                dataGridView2.Rows[q + 1].Cells[p].Value = "кол.д" + Convert.ToString(l - 1);
+                dataGridView2.Rows[q + 2].Cells[p].Value = "остаток" + Convert.ToString(sum);
+                //Debugger.Break();                
+                mas[p] = results.GetRange(0, results.Count);                
+                //Debugger.Break();
+                //dataGridView2.Rows[0].Cells[1].Value = sum;
+                /*обработка и вывод результатов конец*/
+                //GLres.Add("кол.д" + Convert.ToString(l-1));
+                //GLost.Add("остаток" + Convert.ToString(sum));
+            }
+            int min = int.MaxValue;
+            int numb = 0;
+            int list = 0;
+            foreach (List<int> item in mas)
+            {
+                int u = item[item.Count - 2];
+                //Debugger.Break();
+                if (min > item[item.Count - 2])
+                {
+                    min = item[item.Count - 2];                    
+                    list = numb;
+                }
+                numb = numb + 1;
+            }
+            //Debugger.Break();
+            int d = 0;
+            foreach (int ele in mas[list])
+            {
+                dataGridView3.Rows.Add();
+                dataGridView3.Rows[d].Cells[0].Value = ele;
+                    d = d + 1;
+            }
+        }
+
+        private List<int> newaArr(List<int> aArr2)/*сотрируем список по цыклу*/
+        {
+            var tmp = aArr2[aArr2.Count - 1];
+            for (var i = aArr2.Count - 1; i != 0; --i) aArr2[i] = aArr2[i - 1];
+            aArr2[0] = tmp;
+            return aArr2;
+        }        
+        
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {        
+
+        }
+    }
+}
